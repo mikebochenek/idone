af96aead2bd416d526bf80cbb9ec78cf1b465028
mikebochenek <mike.bochenek@gmail.com>
2014-02-18 21:16:14 +0100

    merge front end changes from aptana (html5) into java mvn project

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -1,31 +1,15 @@
-/*
- * JBoss, Home of Professional Open Source
- * Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
- * contributors by the @authors tag. See the copyright.txt in the
- * distribution for a full listing of individual contributors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-function MembersCtrl($scope, $http, Members) {
+function DonesCtrl($scope, $http, Dones) {
 
     // Define a refresh function, that updates the data from the REST service
     $scope.refresh = function() {
-        $scope.members = Members.query();
+        $scope.dones = Dones.query();
     };
 
     // Define a reset function, that clears the prototype newMember object, and
     // consequently, the form
     $scope.reset = function() {
         // clear input fields
-        $scope.newMember = {};
+        $scope.newDone = {};
     };
 
     // Define a register function, which adds the member using the REST service,
@@ -35,7 +19,7 @@ function MembersCtrl($scope, $http, Members) {
         $scope.errorMessages = '';
         $scope.errors = {};
 
-        Members.save($scope.newMember, function(data) {
+        Dones.save($scope.newDone, function(data) {
 
             // mark success on the registration form
             $scope.successMessages = [ 'Member Registered' ];
