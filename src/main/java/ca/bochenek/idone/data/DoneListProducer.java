1f6ec939fa0e50eb0f5deac32444aeef2a401cca
mikebochenek <mike.bochenek@gmail.com>
2014-02-22 21:32:56 +0100

    adding ejb implementations

--- src/main/java/ca/bochenek/idone/data/DoneListProducer.java
+++ src/main/java/ca/bochenek/idone/data/DoneListProducer.java
@@ -1,10 +1,12 @@
 package ca.bochenek.idone.data;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import ca.bochenek.idone.entity.DoneItem;
 
@@ -17,11 +19,32 @@ public class DoneListProducer {
 
     @SuppressWarnings("unchecked")
 	public List<DoneItem> loadAll() {
-    	return em.createQuery("select p from DoneItem p").getResultList();
+    	return em.createQuery("select d from DoneItem d").getResultList();
     }
     
     public DoneItem findById(long id) {
     	return em.find(DoneItem.class, id);
+    }
+    
+    public DoneItem create(String text, Date doneDate, long category) {
+    	DoneItem item = new DoneItem();
+    	item.setText(text);
+    	item.setDate(doneDate);
+    	item.setCategory(category);
+    	item.setOwner(1L); //TODO
+    	return update(item);
+    }
+
+    
+    public DoneItem update(DoneItem item) {
+    	return em.merge(item);
+    }
+    
+    public List<DoneItem> findByDate(Date date) {
+		String dateStr = " where donedate between " + "-01-01' and '" + "-12-31'"; //TODO
+
+    	Query query = em.createQuery("select d from DoneItem d " + dateStr);
+    	return query.getResultList();
     }
 
 }
---
+++ src/main/java/ca/bochenek/idone/data/UserProducer.java
@@ -0,0 +1,75 @@
+package ca.bochenek.idone.data;
+
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import ca.bochenek.idone.entity.User;
+
+@RequestScoped
+public class UserProducer {
+
+	@Inject
+	private EntityManager em;
+
+	/** this is so bad that I can't believe that I am allowing myself to do this */
+	public User login(String username, String password) {
+		Query query = em.createQuery("select u from User u where username = :username");
+		query.setParameter(0, username);
+		User user = (User) query.getSingleResult();
+
+		if (user != null && user.getPassword() != null
+				&& user.getPassword().equals(hash(password))) {
+			return user;
+		} else {
+			return null;
+		}
+	}
+	
+	public boolean isUsernameAlreadyUsed(String username) {
+		Query query = em.createQuery("select u from User u where username = :username");
+		query.setParameter(0, username);
+		User user = (User) query.getSingleResult();
+		return user != null;
+	}
+	
+	public User create(String username, String password) {
+		User user = new User();
+		user.setUsername(username);
+		user.setPassword(hash(password));
+		return update(user);
+	}
+	
+	public User update(User user) {
+		return em.merge(user);
+	}
+
+	private String hash(String input) {
+		final String salt = "$*82xeDIWQ";
+		String string = input + salt;
+		StringBuffer result = new StringBuffer();
+
+		try { 
+			MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+			messageDigest.reset();
+			messageDigest.update(string.getBytes(Charset.forName("UTF8")));
+			final byte[] resultByte = messageDigest.digest();
+
+			for (int i = 0; i < resultByte.length; ++i) {
+				result.append(Integer.toHexString(
+						(resultByte[i] & 0xFF) | 0x100).substring(1, 3));
+			}
+
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+
+		return result.toString();
+	}
+
+}
